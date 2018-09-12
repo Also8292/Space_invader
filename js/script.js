e79b64884ed1d98d5269580c399ae0f7ad6809f6
Also8292 <sowalils1992@gmail.com>
2018-09-12 14:27:16 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-var vaisseau1 = new Strite("images/vaisseau1.png", 100, 100);
-
-var vaisseau2 = new Strite("images/vaisseau2.png", 300, 100);
-
-var vaisseau3 = new Strite("images/vaisseau3.png", 500, 100);
-
-var vaisseau4 = new Strite("images/vaisseau4.png", 700, 100);
-
-var vaisseau5 = new Strite("images/vaisseau5.png", 900, 100);
+var vaisseau1 = new Strite("images/vaisseau1.png", (document.body.clientWidth / 2) - 37, 550);
